4305d2f7834612b8f7b880362d1c48a75c5a3174
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2022-10-09 12:33:35 -0500

    Problem description added

--- 1-twosome/Solution.java
+++ 1-twosome/Solution.java
@@ -1,4 +1,8 @@
 import java.util.HashMap;
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
 import java.util.Map;
 
 class Solution {
