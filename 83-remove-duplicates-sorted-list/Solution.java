764f8a627a1a6ce8e1bd01283f95408cfddf207a
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2022-12-16 13:14:45 -0600

    83 added

---
+++ 83-remove-duplicates-sorted-list/Solution.java
@@ -0,0 +1,26 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode traversalNode = head;
+         
+         while(traversalNode != null) {
+        	 if (traversalNode.next == null) {
+        		 break;
+        	 }
+        	 if (traversalNode.val == traversalNode.next.val) {
+        		 traversalNode.next = traversalNode.next.next;
+        	 } else {
+        		 traversalNode = traversalNode.next;
+        	 }
+         }
+         
+         return head;
+    }
+}
