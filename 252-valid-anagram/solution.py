d48456f815283959126c96c085427598e5c706fb
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2023-10-08 17:55:39 -0500

    Updated the anagram program

--- 252-valid-anagram/solution.py
+++ 252-valid-anagram/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
         for ch in s:
-            if not t:
-                return False
-            
             if ch in t:
                 t = t.replace(ch, '', 1)
             else:
                 return False
-
-        return len(t) == 0
+        return True
